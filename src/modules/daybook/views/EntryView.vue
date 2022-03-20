2783e7b9b7c834b70de7339fc93bf0840e751d8d
Carlos Zapata <cdanielzt@gmail.com>
2022-03-19 19:31:15 -0600

    create get date helper

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -2,9 +2,9 @@
   <div class="entry-title d-flex justify-content-between p-2">
 
       <div>
-          <span class="text-success fs-3 fw-bold">15</span>
-          <span class="mx-1 fs-3">Julio</span>
-          <span class="mx-2 fs-4 fw-light">2022, Sabado</span>
+          <span class="text-success fs-3 fw-bold">{{ day }}</span>
+          <span class="mx-1 fs-3">{{ month }}</span>
+          <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
       </div>
 
       <div>
@@ -22,7 +22,9 @@
   <hr>
 
   <div class="d-flex flex-column px-3 h-75">
-      <textarea placeholder="¿Qué sucedió hoy?"></textarea>
+      <textarea 
+      v-model="entry.text"
+      placeholder="¿Qué sucedió hoy?"></textarea>
   </div>
 
 
@@ -41,11 +43,19 @@
 import { defineAsyncComponent } from '@vue/runtime-core'
 import { mapGetters } from 'vuex'
 
+import getDayMonthYear from '../helpers/getDayMonthYear'
+
 export default {
     props: {
         id: {
             type: String,
             required: true
+        }
+    },
+
+    data() {
+        return {
+            entry: null
         }
     },
 
@@ -55,12 +65,27 @@ export default {
 
     computed: {
         ...mapGetters('journal',[ 'getEntryById' ]),
+        day() {
+            const { day } = getDayMonthYear( this.entry.date )
+            return day
+        },
+        month() {
+            const { month } = getDayMonthYear( this.entry.date )
+            return month
+        },
+        yearDay() {
+            const { yearDay } = getDayMonthYear( this.entry.date )
+            return yearDay
+        }
     },
 
     methods: {
         loadEntry() {
             const entry = this.getEntryById(this.id)
-            console.log(entry);
+            
+            if( !entry ) this.$router.push({name: 'no-entry'})
+
+            this.entry = entry
         }
     },
 
