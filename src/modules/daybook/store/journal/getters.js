d2885d5404399e70d97a6dd7871f6a81205aee0b
Carlos Zapata <cdanielzt@gmail.com>
2022-03-19 19:17:03 -0600

    get entry by id

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -11,6 +11,13 @@ export const getEntriesByTerm = ( state ) => ( term = '') => {
 
 }
 
-export const getEntriesById = ( /*{ commit }*/) => {
-    
+
+export const getEntryById = ( state ) => ( id = '' ) => {
+
+    const entry = state.entries.find( entry => entry.id === id )
+
+    if( !entry ) return
+
+    return { ...entry } //TODO probar
+
 }
